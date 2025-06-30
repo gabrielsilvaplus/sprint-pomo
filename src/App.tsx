abc105156a144d873a5ce0caa4b9e9f96a99b5d0
Gabriel Pereira <gabrieldasilvapp@gmail.com>
2025-06-30 11:29:07 -0300

    chore: cria estrutura HTML e CSS inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
-    <div>
-      <Heading>
-        Olá Mundo!
-        <button>
-          <TimerIcon></TimerIcon>
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-        repudiandae incidunt recusandae tempora cum officiis dolorem mollitia
-        quam minus cupiditate culpa eveniet tempore. Excepturi vero quae culpa
-        similique officia nulla! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Libero nihil cumque inventore atque obcaecati fugiat
-        fugit dolores, dicta a, porro non temporibus. Voluptatum eos vero
-        laudantium non laborum in officiis?
-      </p>
-    </div>
+    <>
+      <div className='container'>
+        <div className='content'>
+          <section>Logo</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>Menu</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>Form</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>Footer</section>
+        </div>
+      </div>
+    </>
   );
 }
